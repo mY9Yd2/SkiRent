a559be5859a8b58f494f30b789bb845b060ae7aa
Kovács József Miklós <bsze3kovjoz@vasvari.org>
2025-02-13 08:44:44 +0100

    BaseRepository és IRepository létrehozása

---
+++ src/SkiRent.Api/Data/Repositories/BaseRepository.cs
@@ -0,0 +1,57 @@
+﻿using System.Linq.Expressions;
+
+using Microsoft.EntityFrameworkCore;
+
+namespace SkiRent.Api.Data.Repositories;
+
+public abstract class BaseRepository<T> : IRepository<T> where T : class
+{
+    protected readonly DbContext _context;
+    protected readonly DbSet<T> _dbSet;
+
+    protected BaseRepository(DbContext context)
+    {
+        _context = context;
+        _dbSet = _context.Set<T>();
+    }
+
+    public virtual async Task<T?> GetByIdAsync(int id)
+    {
+        return await _dbSet.FindAsync(id);
+    }
+
+    public virtual async Task<IEnumerable<T>> GetAllAsync()
+    {
+        return await _dbSet.ToListAsync();
+    }
+
+    public virtual async Task<T?> FindAsync(Expression<Func<T, bool>> predicate)
+    {
+        return await _dbSet.Where(predicate).FirstOrDefaultAsync();
+    }
+
+    public virtual async Task<IEnumerable<T>> FindAllAsync(Expression<Func<T, bool>> predicate)
+    {
+        return await _dbSet.Where(predicate).ToListAsync();
+    }
+
+    public virtual async Task AddAsync(T entity)
+    {
+        await _dbSet.AddAsync(entity);
+    }
+
+    public virtual void Update(T entity)
+    {
+        _dbSet.Update(entity);
+    }
+
+    public virtual void Delete(T entity)
+    {
+        _dbSet.Remove(entity);
+    }
+
+    public virtual async Task<bool> ExistsAsync(Expression<Func<T, bool>> predicate)
+    {
+        return await _dbSet.AnyAsync(predicate);
+    }
+}
---
+++ src/SkiRent.Api/Data/Repositories/IRepository.cs
@@ -0,0 +1,15 @@
+﻿using System.Linq.Expressions;
+
+namespace SkiRent.Api.Data.Repositories;
+
+public interface IRepository<T> where T : class
+{
+    public Task<T?> GetByIdAsync(int id);
+    public Task<IEnumerable<T>> GetAllAsync();
+    public Task<T?> FindAsync(Expression<Func<T, bool>> predicate);
+    public Task<IEnumerable<T>> FindAllAsync(Expression<Func<T, bool>> predicate);
+    public Task AddAsync(T entity);
+    public void Update(T entity);
+    public void Delete(T entity);
+    public Task<bool> ExistsAsync(Expression<Func<T, bool>> predicate);
+}
