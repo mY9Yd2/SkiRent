fdd374cd6b667337da2a39a77acee044205e4e1f
Kovács József Miklós <bsze3kovjoz@vasvari.org>
2025-02-13 20:12:14 +0100

    seed.sql hozzáadása

---
+++ src/SkiRent.Database/seed.sql
@@ -0,0 +1,2 @@
+INSERT INTO Users (Email, PasswordHash, UserRole) VALUES
+('admin@example.com', 'AQAAAAIAAYagAAAAEDtCTpS9GBrTvvsFJgv+0P7StQA+mb0JjOYh2rvn3WVWAP7ib79kDRcH/qqjglIiGg==', 'admin'); -- Password: admin
