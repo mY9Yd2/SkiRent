916ccc73a605fabf75fa7617089812a1ed6c7e82
Kovács József Miklós <bsze3kovjoz@vasvari.org>
2025-02-13 08:52:33 +0100

    BaseController és UnhandledErrorException létrehozása

---
+++ src/SkiRent.Api/Controllers/Base/BaseController.cs
@@ -0,0 +1,20 @@
+﻿using FluentResults;
+
+using Microsoft.AspNetCore.Mvc;
+
+using SkiRent.Api.Exceptions;
+
+namespace SkiRent.Api.Controllers.Base;
+
+public abstract class BaseController : ControllerBase
+{
+    [NonAction]
+    public ObjectResult Problem(IError error)
+    {
+        switch (error)
+        {
+            default:
+                throw new UnhandledErrorException($"Unhandled error type: {error.GetType().Name}.");
+        }
+    }
+}
---
+++ src/SkiRent.Api/Exceptions/UnhandledErrorException.cs
@@ -0,0 +1,13 @@
+﻿namespace SkiRent.Api.Exceptions;
+
+public class UnhandledErrorException : Exception
+{
+    public UnhandledErrorException()
+    { }
+
+    public UnhandledErrorException(string message) : base(message)
+    { }
+
+    public UnhandledErrorException(string message, Exception inner) : base(message, inner)
+    { }
+}
