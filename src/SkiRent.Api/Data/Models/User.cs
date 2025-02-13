565c57744f06270c57fdd084cdd8f7adb51f3dbd
Kovács József Miklós <bsze3kovjoz@vasvari.org>
2025-02-13 15:20:34 +0100

    adatbázis scaffoldolása

---
+++ src/SkiRent.Api/Data/Models/User.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using Microsoft.EntityFrameworkCore;
+
+namespace SkiRent.Api.Data.Models;
+
+[Table("users")]
+[Index("Email", Name = "Email", IsUnique = true)]
+public partial class User
+{
+    [Key]
+    [Column(TypeName = "int(11)")]
+    public int Id { get; set; }
+
+    [StringLength(100)]
+    public string Email { get; set; } = null!;
+
+    [StringLength(255)]
+    public string PasswordHash { get; set; } = null!;
+
+    [Column(TypeName = "enum('admin','customer')")]
+    public string UserRole { get; set; } = null!;
+}
---
+++ src/SkiRent.Api/Data/SkiRentContext.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.EntityFrameworkCore;
+using SkiRent.Api.Data.Models;
+
+namespace SkiRent.Api.Data;
+
+public partial class SkiRentContext : DbContext
+{
+    public SkiRentContext(DbContextOptions<SkiRentContext> options)
+        : base(options)
+    {
+    }
+
+    public virtual DbSet<User> Users { get; set; }
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder
+            .UseCollation("utf8mb4_unicode_ci")
+            .HasCharSet("utf8mb4");
+
+        modelBuilder.Entity<User>(entity =>
+        {
+            entity.HasKey(e => e.Id).HasName("PRIMARY");
+
+            entity.Property(e => e.UserRole).HasDefaultValueSql("'customer'");
+        });
+
+        OnModelCreatingPartial(modelBuilder);
+    }
+
+    partial void OnModelCreatingPartial(ModelBuilder modelBuilder);
+}
