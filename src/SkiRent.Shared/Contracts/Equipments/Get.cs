9b7050d48cc78dfe03423c455f6ebacf2ae88f0f
Kovács József Miklós <bsze3kovjoz@vasvari.org>
2025-02-28 09:00:27 +0100

    IsAvailable elírás javítása

--- src/SkiRent.Shared/Contracts/Equipments/Get.cs
+++ src/SkiRent.Shared/Contracts/Equipments/Get.cs
@@ -14,6 +14,6 @@
 
         public required int AvailableQuantity { get; init; }
 
-        public bool IsAvaiable => AvailableQuantity > 0;
+        public bool IsAvailable => AvailableQuantity > 0;
     }
 }
