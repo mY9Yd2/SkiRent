e1f6cfe092065d5f27a20175f43e715f4c8c54c4
Kovács József Miklós <bsze3kovjoz@vasvari.org>
2025-03-20 09:23:09 +0100

    MahApps.Metro és FontAwesome használata az aszt. alk.ban

--- src/SkiRent.Desktop/Views/Main/MainWindow.xaml.cs
+++ src/SkiRent.Desktop/Views/Main/MainWindow.xaml.cs
@@ -1,4 +1,4 @@
-﻿using System.Windows;
+﻿using MahApps.Metro.Controls;
 
 using SkiRent.Desktop.ViewModels.Main;
 
@@ -7,7 +7,7 @@ namespace SkiRent.Desktop.Views.Main
     /// <summary>
     /// Interaction logic for MainWindow.xaml
     /// </summary>
-    public partial class MainWindow : Window
+    public partial class MainWindow : MetroWindow
     {
         public MainWindow()
         {
